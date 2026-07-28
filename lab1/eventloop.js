9ed8ab96dc49590fd8cc96d490545c7d1b494725
akshat <akshatovo@gmail.com>
2026-07-28 14:40:49 +0530

    eventloop done

---
+++ lab1/eventloop.js
@@ -0,0 +1,35 @@
+import { writeFile } from "fs/promises";
+
+const f1=()=>{
+
+    console.log(`f1`)
+}
+const f2=()=>{
+    console.log(`f2`)
+}
+const f3=()=>{
+    console.log(`f3`)
+}
+const writedata =async()=>{
+    await writeFile('note.txt' , "I am FS module") ;
+    console.log(`file written`) ;
+
+}
+const main=()=>{
+    console.log(`main`) ;
+    writedata() ;
+    //f1() ;
+    setTimeout(f2,0) ;
+    //setInterval(f2,1000) ;
+    setImmediate(f3) ;
+    process.nextTick(f1) ;
+    console.log(`end`) ;
+    new Promise((resolve , reject)=>{
+        console.log("I am promise 1") ;
+    })
+        new Promise((resolve , reject)=>{
+        console.log("I am promise 2") ;
+    })
+    
+};
+main() ;
